d6722bb5703769decc108cf7a874277730f4b5fe
lyMa <lymt@pixon.games>
2026-06-27 17:01:34 +0700

    test

---
+++ Assets/ROOT/Tool/Editor/RequiredNodeView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RequiredNodeView: Node
+{
+        public Port InputPort { get; private set; }
+        
+        private static readonly Color NodeColor = new Color(0.45f, 0.08f, 0.08f);
+
+        public RequiredNodeView()
+        {
+                title = "■  RequiredNodeView";
+                titleContainer.style.backgroundColor = new StyleColor(NodeColor);
+                
+        }
+}
